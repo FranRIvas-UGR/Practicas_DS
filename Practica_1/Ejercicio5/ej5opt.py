705595acdd3acde0045315341bf7c84a0ff4b22a
Fran Rivas <franciscorivas@correo.ugr.es>
2024-03-06 17:14:49 +0100

    Ejercicio 5 BeautifulSoup

--- Practica_1/Ejercicio5/ej5opt.py
+++ Practica_1/Ejercicio5/ej5opt.py
@@ -21,12 +21,40 @@ class BeautifulSoupStrategy(ScrapeStrategy):
             soup = BeautifulSoup(response.content, 'html.parser')
             open_value_td = soup.find('td', {'data-test': 'OPEN-value'})
             close_value_td = soup.find('td', {'data-test': 'PREV_CLOSE-value'})
-            if open_value_td:
-                return open_value_td.text.strip()
-            else:
+            volume_value_td = soup.find('td', {'data-test': 'TD_VOLUME-value'})
+            market_cap_value_td = soup.find('td', {'data-test': 'MARKET_CAP-value'})
+
+            if not open_value_td:
                 return 'Open Value not found'
+
+            if not close_value_td:
+                return 'Close Value not found'
+
+            if not volume_value_td:
+                return 'Volume Value not found'
+            
+            if not market_cap_value_td:
+                return 'Market Cap Value not found'
+            
+            open_value = open_value_td.text.strip()
+            close_value = close_value_td.text.strip()
+            volume = volume_value_td.text.strip()
+            market_capitalization = market_cap_value_td.text.strip()
+            return open_value, close_value, volume, market_capitalization
+            
         else:
             return f'Failed to retrieve the webpage, status code: {response.status_code}'
+                
+    def export_to_json(self, data, filename):
+        # Data es una lista de open_value, close_value, volume, market_capitalization
+        with open(filename, 'w') as file:
+            file.write('{\n')
+            file.write(f'  "Precio de apertura": "{data[0]}",\n')
+            file.write(f'  "Precio de cierre anterior": "{data[1]}",\n')
+            file.write(f'  "Volumen": "{data[2]}",\n')
+            file.write(f'  "Capitalización de Mercado": "{data[3]}"\n')
+            file.write('}\n')
+        return filename
 
 
 class SeleniumStrategy(ScrapeStrategy):
@@ -93,10 +121,21 @@ class Context:
 
 
 url = 'https://finance.yahoo.com/quote/TSLA'
+
 context = Context(SeleniumStrategy())
+context2 = Context(BeautifulSoupStrategy())
+
 open_value, close_value, volume, market_capitalization = context.scrape(url)
+open_value2, close_value2, volume2, market_capitalization2 = context2.scrape(url)
 
 data = [open_value, close_value, volume, market_capitalization]
+data2 = [open_value2, close_value2, volume2, market_capitalization2]
+
 filename = sys.argv[1]
+filename2 = sys.argv[2]
+
 context._strategy.export_to_json(data, filename)
+context2._strategy.export_to_json(data2, filename2)
+
 print(f'Exporto a {filename}')
+print(f'Exporto a {filename2}')
